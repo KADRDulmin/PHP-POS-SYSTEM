d9b8047e553f53d22d6923f14d3b336c3f542efa
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2025-01-06 12:25:46 +0530

    Progress

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# PHP-POS-SYSTEM
- 
+# IMR-Project
---
+++ dbcon.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$db = "possystem";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
